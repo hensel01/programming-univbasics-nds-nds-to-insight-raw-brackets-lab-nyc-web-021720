da4b6711a22aa2bd663e8f2b74771f7c64def2e6
Eliot Henson <eliotrhenson@gmail.com>
2020-02-12 18:29:55 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++
@@ -1,9 +0,0 @@
-rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
-  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-end	
-
-def directors_totals(source)
-  result = {}
-  director_index = 0 
-  result
-end
